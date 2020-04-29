4a6d3fe28ff6cf75657763b0758423e73521a0af
Tiago <trodriguesr@gmail.com>
2020-04-28 23:45:58 -0300

    first commit

---
+++ src/Aplicacao.java
@@ -0,0 +1,11 @@
+import net.tiago.cm.model.Tabuleiro;
+import net.tiago.cm.view.TabuleiroConsole;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 3);
+		new TabuleiroConsole(tabuleiro);
+	}
+}
